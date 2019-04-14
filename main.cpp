dac00c99d2b9e915058806f044120be8429621ac
Kopantsho Mathafa <849038@students.wits.ac.za>
2019-04-14 22:18:14 +0200

    Final lab without MR

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ int main()
 {
 
     try {
+    
+    //Performing operations for File1ForLab3.txt
 	wordCounter myCounter;
 	myCounter.getWordsFromFile("File1ForLab3.txt");
 	myCounter.printWordList();
@@ -19,6 +21,18 @@ int main()
 	
 	InvertedText invT(myCounter.wordList(), "File1ForLab3.txt");
 	invT.FindWordLines();
+	
+	//Performing operations for File2ForLab3.txt
+	myCounter.getWordsFromFile("File2ForLab3.txt");
+	myCounter.printWordList();
+	
+	K = 10;
+	myCounter.printTopKWords(K);
+	K = 20;
+	myCounter.printTopKWords(K);
+	
+	InvertedText invText(myCounter.wordList(), "File2ForLab3.txt");
+	invText.FindWordLines();
     } catch(const char* e) {
 	cout << e << endl;
     }
